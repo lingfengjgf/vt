6680225dd1288d93e8612f06f3bcd30594d21c29
lingfengjgf <45424704+lingfengjgf@users.noreply.github.com>
2018-12-14 22:13:29 +0800

    carousel1

--- ViewTastes/vt/js/vt_index.js
+++ ViewTastes/vt/js/vt_index.js
@@ -11,6 +11,35 @@ function turn(){
         showArt.parentNode.children[0].className="show";
     }
 }
+
 var n=setInterval(turn,3000);
+c1.onmouseover=function(e){
+    if(e.target.nodeName=='A'||e.target.nodeName=='IMG'||e.target.nodeName=='P')
+        clearInterval(n);
+}
+c1.onmouseout=function(e){
+    if(e.target.nodeName=='A'||e.target.nodeName=='IMG'||e.target.nodeName=='P')
+        n=setInterval(turn,3000);
+}
+var showImgs=document.querySelectorAll('#c1>div>a>img');
+var as=document.querySelectorAll('#c1>div>a');
+var showArts=document.querySelectorAll('#c1>article');
+for(var show of showImgs){
+    show.onmouseover=function() {
+        var img = this;
+        if (img.className != 'show') {
+            var showImg = document.querySelector('#c1>div>a>img.show');
+            showImg.className='';
+            img.className='show';
+            var showArt = document.querySelector('#c1>article.show');
+            for(var i=0;i<showImgs.length;i++){
+                if(img==showImgs[i])
+                    break;
+            }
+            showArt.className = '';
+            showArts[i].className='show';
+        }
+    }
+}
 
  
